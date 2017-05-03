c721b619d18d59770e7ed3aa06c9cdba514b2567
wangathan <wangathan@gmail.com>
2017-05-03 13:06:38 -0400

    batched drawing EOSD samples

---
+++ batchRunSampleTileE.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+qsub runSampleTileE.sh Bh11v10
+qsub runSampleTileE.sh Bh11v12
+
+qsub runSampleTileE.sh Bh12v10
+qsub runSampleTileE.sh Bh12v11
+qsub runSampleTileE.sh Bh12v12
+
+qsub runSampleTileE.sh Bh10v10
+qsub runSampleTileE.sh Bh10v11
+qsub runSampleTileE.sh Bh10v12
